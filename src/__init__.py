ecb92a594d404e620efe40b3c7acba728622bd92
formegusto <formegusto@gmail.com>
2022-04-12 11:23:40 +0900

    update pt2 : documentation comp

--- src/__init__.py
+++ src/__init__.py
@@ -3,4 +3,5 @@ from src.PublicPredictor import PublicPredictor
 from src.PrevPublicPredictor import PublicPredictor as PrevPublicPredictor, get_col_list, get_step
 
 __version__ = "1.0.0"
-__all__ = ["crs", "PublicPredictor", "PrevPublicPredictor", "get_col_list", "get_step"]
+__all__ = ["crs", "PublicPredictor",
+           "PrevPublicPredictor", "get_col_list", "get_step"]
